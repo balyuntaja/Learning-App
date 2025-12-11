8713d068ec58e9d9d0bd4e641548581361c29a72
gunghimel <sajjadul.himel@gmail.com>
2025-12-11 23:49:37 +0800

    Update dashboardRoutes.js

--- src/routes/dashboardRoutes.js
+++ src/routes/dashboardRoutes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { getDashboard } from "../controllers/dashboardController.js";
+import { authMiddleware } from "../middleware/auth.js";
 
 const router = express.Router();
 
