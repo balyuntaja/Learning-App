dbe18e0e906abcdda27d290f26af3451cb7bb675
gunghimel <sajjadul.himel@gmail.com>
2025-12-11 23:46:05 +0800

    Update jwt.js

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -1,1 +1,13 @@
+import jwt from "jsonwebtoken";
+
+const JWT_SECRET = process.env.JWT_SECRET || "secret123";
+const JWT_EXPIRES = "7d";
+
+export function generateToken(payload) {
+  return jwt.sign(payload, JWT_SECRET, { expiresIn: JWT_EXPIRES });
+}
+
+export function verifyToken(token) {
+  return jwt.verify(token, JWT_SECRET);
+}
 
